e076e1e372315c58e3d078bc68647caccfc2314d
tdelorme <tdelorme@MM316>
2016-02-01 10:18:37 +0100

    test2

---
+++ src/test/menu.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.swing.JFileChooser;
+
+
+class menu {
+	public static void main(String[] arg) throws IOException {
+		JFileChooser dialogue = new JFileChooser(new File("."));
+		PrintWriter sortie;
+		File fichier;
+
+		if (dialogue.showOpenDialog(null)== 
+				JFileChooser.APPROVE_OPTION) {
+			fichier = dialogue.getSelectedFile();
+			sortie = new PrintWriter
+					(new FileWriter(fichier.getPath(), true));
+			sortie.println(arg[0]);
+			sortie.close();
+		}
+	}
+}
+
