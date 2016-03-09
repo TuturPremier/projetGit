e5874bdec8464b9dda557f774f30164f64ebc11e
tdelorme <tdelorme@BB205>
2016-03-09 15:41:59 +0100

    test de presentation

---
+++ src/Tableur.java
@@ -0,0 +1,52 @@
+
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.event.TableModelListener;
+import javax.swing.event.TableModelEvent;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.text.DecimalFormat;
+
+public class Tableur extends JFrame implements TableModelListener {
+	JTable table;
+	DefaultTableModel tableur;
+
+	public Tableur() { 
+		tableur = new  DefaultTableModel();
+		tableur.addColumn("fichier");
+		tableur.addColumn("dossier");
+		
+		tableur.setRowCount(10);
+
+		table = new JTable(tableur);
+		table.setPreferredScrollableViewportSize(new Dimension(300, 200));
+		add(new JScrollPane(table), BorderLayout.CENTER);
+		tableur.addTableModelListener(this);
+		
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocation(200, 300);
+		pack(); 
+		setVisible(true); 
+		////////////////////////////////////////////////////////////////////
+		
+		
+		
+	}
+
+	@Override
+	public void tableChanged(TableModelEvent arg0) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	
+	
+	
+
+} 
+
